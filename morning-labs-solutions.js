ca0bd5da36be06739c3ac8b23cb9042050f14d23
Your Name <controlsltrapidfire@gmail.comm>
2017-12-06 12:24:20 -0600

    Sections one and two

--- morning-labs-solutions.js
+++ morning-labs-solutions.js
@@ -5,17 +5,40 @@
 // Write your answers to the conceptual questions
 
 // 1. How do we assign a value to a variable?
-
+1. with a single equals sign.
+like this:
+let variable = "assigned value";
+example:
+let name = "Christopher";
 
 // 2. How do we change the value of a variable?
-
+2. you just name the varible and make it equal something else
+like this:
+name = "Hanten";
+now the variable name is 'Hanten' instead of 'Christopher.'
 
 // 3. How do we assign an existing variable to a new variable?
-
+3. by adding the existing varible to the right of the equals sign with the new
+variable name to the left.
+like this:
+let Kname = name;
 
 ///////////////////////////////////////////////////
 
 //Write the answers to your String exercises here:
+/*
+let firstVariable = "Hello World";
+firstVariable = 23;
+let secondVariable = firstVariable;
+secondVariable = "Goodbye World";
+console.log(firstVariable);
+*/
+1. 23
+2.
+let yourName = "Christopher";
+let intro = "Hello, my name is ";
+console.log(intro + yourName);
+
 
 
 
